2aa6c96f434caf108105ef43241d4581adb11e55
Kota Kasahara <kota.kasahara@protein.osaka-u.ac.jp>
2015-06-10 11:11:24 +0900

    ver.0.35.f.7 nb15off on shared mem

--- src/cuda/cuda_setup.h
+++ src/cuda/cuda_setup.h
@@ -28,8 +28,9 @@
 
 cudaStream_t stream_pair_home;
 
-#define D_N_ATOM_CELL  8
-#define D_N_ATOM_CELL_3  24
+#define N_ATOM_CELL  8
+#define N_ATOM_CELL_3  24
+#define MAX_N_CELL_BLOCK 16
 
 #define WARPSIZE 32
 
