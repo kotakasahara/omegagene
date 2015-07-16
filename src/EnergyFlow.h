73e847284ad7dffa3172b74f24adcf3a93b4180d
Benson Ma <bm@berkeley.edu>
2015-07-16 13:18:34 +0900

    Minor spelling fix for vector (初めてのコミット！)

--- src/EnergyFlow.h
+++ src/EnergyFlow.h
@@ -12,23 +12,23 @@ class EnergyFlow : public CelesteObject{
   static int n_quad_terms;
  public:
   int n_atoms;
-  vecotr< pair<int,int> > pair_terms_atoms;
-  vecotr<real> pair_terms_ene;
-  vecotr<real> pair_terms_work;
+  vector< pair<int,int> > pair_terms_atoms;
+  vector<real> pair_terms_ene;
+  vector<real> pair_terms_work;
   vector<real> pair_ene_i;
   vector<real> pair_eneflo_kine;
   vector<real> pair_eneflo_pote;
 
-  vecotr< trio<int,int,int> >  trio_terms_atoms;
-  vecotr<real> trio_terms_ene;
-  vecotr<real> trio_terms_work;
+  vector< trio<int,int,int> >  trio_terms_atoms;
+  vector<real> trio_terms_ene;
+  vector<real> trio_terms_work;
   vector<real> trio_ene_i;
   vector<real> trio_eneflo_kine;
   vector<real> trio_eneflo_pote;
 
-  vecotr<real> quad_terms_atoms;
-  vecotr<real> quad_terms_ene;
-  vecotr<real> quad_terms_work;
+  vector<real> quad_terms_atoms;
+  vector<real> quad_terms_ene;
+  vector<real> quad_terms_work;
   vector<real> quad_ene_i;
   vector<real> quad_eneflo_kine;
   vector<real> quad_eneflo_pote;
@@ -37,7 +37,7 @@ class EnergyFlow : public CelesteObject{
   int initial(int in_n_atoms);
   int reserve_values();
   int reset_values();
-  
+
 };
 
 #endif
