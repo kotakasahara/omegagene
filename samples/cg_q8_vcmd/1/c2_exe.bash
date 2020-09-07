be4a54b3f93c1d62a04a5e61078aa19cbca876b6
Kota Kasahara <ktkshr@fc.ritsumei.ac.jp>
2020-09-07 12:10:56 +0900

    ver.0.52.m : Bugfix for sample files, cg_q8_vcmd

--- samples/cg_q8_vcmd/1/c2_exe.bash
+++ samples/cg_q8_vcmd/1/c2_exe.bash
@@ -12,7 +12,7 @@ OMEGATK=${HOME}/local/omegagene/toolkit
 while [ $SERIES -le $N_SERIES ]
 do 
     cd n$SERIES
-    python ${OMEGATK}/mdinput_generator.py -i md.inp -o md.inp.cls -v v.0.49 > log_inputgen.txt
+    python ${OMEGATK}/mdinput_generator.py -i md.inp -o md.inp.cls -v v.0.52 > log_inputgen.txt
     ${OMEGABIN} --cfg md.inp.run --inp md.inp.cls > md.out 
     SERIES=$(( $SERIES + 1 ))
     cd ..
